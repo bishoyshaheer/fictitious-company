598d3bca601068c870beaf0d51bc1c2f0cd18378
bishoyshaheer <bishoy.shaheer@gmail.com>
2016-05-18 14:15:22 +0200

    HR core spring project skeleton

---
+++ sources/fictitious-company/hr-core/src/main/java/com/fc/hr/HrCoreApplication.java
@@ -0,0 +1,12 @@
+package com.fc.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrCoreApplication.class, args);
+	}
+}
