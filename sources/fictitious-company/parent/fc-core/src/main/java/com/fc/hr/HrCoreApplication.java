9d7a196d313c05d905422e62353f330d9a7c57eb
bishoyshaheer <bishoy.shaheer@gmail.com>
2016-05-21 23:16:57 +0200

    parent POM added

--- sources/fictitious-company/parent/fc-core/src/main/java/com/fc/hr/HrCoreApplication.java
+++ sources/fictitious-company/parent/fc-core/src/main/java/com/fc/hr/HrCoreApplication.java
@@ -2,11 +2,13 @@ package com.fc.hr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.SocketUtils;
 
 @SpringBootApplication
 public class HrCoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrCoreApplication.class, args);
+		System.out.println("hello world");
 	}
 }
