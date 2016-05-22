867ccab37032004095878d6bc794e482f0b428dd
bishoyshaheer <bishoy.shaheer@gmail.com>
2016-05-22 17:04:18 +0200

    test spring boot service

--- sources/fictitious-company/parent/fc-core/src/main/java/com/fc/hr/HrCoreApplication.java
+++ sources/fictitious-company/parent/fc-core/src/main/java/com/fc/hr/HrCoreApplication.java
@@ -1,14 +1,25 @@
 package com.fc.hr;
 
+import org.springframework.boot.Banner.Mode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.SocketUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HrCoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrCoreApplication.class, args);
-		System.out.println("hello world");
+	@RequestMapping("/bisho")
+	public String home(){
+		return "hello world\nbishoy is here8";
 	}
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication app = new SpringApplication(HrCoreApplication.class);
+		app.setBannerMode(Mode.OFF);
+		app.run(args);
+		System.out.println("hello world\nbishoy is here");
+	}
+	
 }
