b3ce6412259323c251efc071dd36dfda0ab84016
bishoyshaheer <bishoy.shaheer@gmail.com>
2016-05-31 15:10:28 +0200

    angular 2 as client app

--- sources/fictitious-company/fc-parent/fc-web-client/Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.initConfig({
-        jshint: {
-            files: ['Gruntfile.js', 'src/main/resources/javascript/*.js','src/test/resources/javascript/*.js']
-        }
-    });
-    grunt.registerTask('default', ['jshint']);
-};
