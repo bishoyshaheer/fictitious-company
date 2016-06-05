74381dd707e7155ce4566b9f559b8f39b5976005
bishoyshaheer <bishoy.shaheer@gmail.com>
2016-06-05 18:05:24 +0200

    angular login component

--- sources/fictitious-company/fc-parent/fc-web-client/app/app.component.js
+++ sources/fictitious-company/fc-parent/fc-web-client/app/app.component.js
@@ -9,14 +9,27 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
+var login_component_1 = require('./components/login/login.component');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Fictitious Company';
     }
     AppComponent = __decorate([
         core_1.Component({
-            selector: 'my-app',
-            template: "<div class=\"container\">\n    <div class=\"row\">\n        <div class=\"col-md-4 col-md-offset-4\">\n            <div class=\"login-panel panel panel-default\">\n                <div class=\"panel-heading\">\n                    <h3 class=\"panel-title\">Please Sign In</h3>\n                </div>\n                <div class=\"panel-body\">\n                    <form role=\"form\">\n                        <fieldset>\n                            <div class=\"form-group\">\n                                <input class=\"form-control\" placeholder=\"E-mail\" name=\"email\" type=\"email\" autofocus>\n                            </div>\n                            <div class=\"form-group\">\n                                <input class=\"form-control\" placeholder=\"Password\" name=\"password\" type=\"password\" value=\"\">\n                            </div>\n                            <div class=\"checkbox\">\n                                <label>\n                                    <input name=\"remember\" type=\"checkbox\" value=\"Remember Me\">Remember Me\n                                </label>\n                            </div>\n                            <!-- Change this to a button or input when using this as a form -->\n                            <a href=\"index.html\" class=\"btn btn-lg btn-success btn-block\">Login</a>\n                        </fieldset>\n                    </form>\n                </div>\n            </div>\n        </div>\n    </div>\n</div>"
-        }), 
+            selector: 'fc-app',
+            template: "Loading...",
+            directives: [router_1.ROUTER_DIRECTIVES],
+            providers: [
+                router_1.ROUTER_PROVIDERS
+            ]
+        }),
+        router_1.Routes([
+            {
+                path: '/login',
+                component: login_component_1.LoginComponent
+            }
+        ]), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
---
+++ sources/fictitious-company/fc-parent/fc-web-client/app/components/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'fc-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+
+}
